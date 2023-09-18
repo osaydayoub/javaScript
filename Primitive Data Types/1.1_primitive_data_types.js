dc88554e515d0fcb4453936c2cd7f1b7a2577729
osaydayoub <osaydayoub@gmail.com>
2023-09-18 14:14:35 +0300

    1.1

---
+++ Primitive Data Types/1.1_primitive_data_types.js
@@ -0,0 +1,15 @@
+let stringVar = "string";
+console.log(stringVar);
+let numberVar = 10;
+console.log(numberVar);
+let objectVar = Math;
+console.log(objectVar);
+let booleanVar= true;
+console.log(booleanVar);
+let nullVar= null;
+console.log(nullVar);
+let undVar;
+console.log(undVar);
+const mySymbol = Symbol();
+console.log(mySymbol);
+
