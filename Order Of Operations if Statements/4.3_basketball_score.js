1617897560cd12ff24f0951d18dba81b9376d681
osaydayoub <osaydayoub@gmail.com>
2023-09-18 20:18:32 +0300

    4.3

---
+++ Order Of Operations if Statements/4.3_basketball_score.js
@@ -0,0 +1,17 @@
+let JohnTeamAvg=(89+120+103)/3;
+let MikeTeamAvg=(116+94+123)/3;
+if(JohnTeamAvg>MikeTeamAvg){
+    console.log("John's team wins!");
+}else if(JohnTeamAvg<MikeTeamAvg){
+    console.log("Mike's team wins!");
+}else{
+    console.log("it's a draw!");
+}
+JohnTeamAvg=(130+120+103)/3;
+if(JohnTeamAvg>MikeTeamAvg){
+    console.log("John's team wins!");
+}else if(JohnTeamAvg<MikeTeamAvg){
+    console.log("Mike's team wins!");
+}else{
+    console.log("it's a draw!");
+}
