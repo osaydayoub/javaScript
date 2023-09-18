a2df747f808fba528fece09ff73a08fffb4ffa5b
osaydayoub <osaydayoub@gmail.com>
2023-09-18 14:34:01 +0300

    1.2

---
+++ Primitive Data Types/1.2.js
@@ -0,0 +1,33 @@
+//------Submit your answers under the
+//question------//
+//create three different ways to declare variables
+let x;
+var y;
+const z=10;
+//declare a variable and reassign a value to it
+let a;
+a=100;
+
+//create a variable and after that assign a string
+//with its value being: He's got it!
+let stringVar;
+stringVar="He's got it!";
+
+/*
+1. create a variable and assign a value on how
+much a restaurant bill is.
+2. create a variable and assign a value on how
+much tax they should pay.
+3. create a variable that will calculate the bill
+* tax and its output would be: Your total bill is
+<total bill> $.
+*/
+let restaurantBill=1000;
+let tax=0.1;
+let totalBill=tax*restaurantBill+restaurantBill;
+
+
+// Round the number 50.6 to its nearest integer
+Math.round(50.6);
+//Create a variable that is undefined
+let undVar;
