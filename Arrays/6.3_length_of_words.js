731ab533f14418ca54c3c4c345a6c2c74fca2b7a
osaydayoub <osaydayoub@gmail.com>
2023-09-19 11:34:54 +0300

    6.3

---
+++ Arrays/6.3_length_of_words.js
@@ -0,0 +1,8 @@
+let stringsArray=['a','aaaa','aa','ccccccc','qwqwwqwqwqwq'];
+let  lengthsArray=[stringsArray[0].length,
+stringsArray[1].length,
+stringsArray[2].length,
+stringsArray[3].length,
+stringsArray[4].length
+];
+console.log(lengthsArray);
