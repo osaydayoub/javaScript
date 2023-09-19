0e0859ef4f9c25809614e75d11f0e8c386e238a2
osaydayoub <osaydayoub@gmail.com>
2023-09-19 15:56:06 +0300

    9.2

--- for and while loops/9.2_while_loop.js
+++ for and while loops/9.2_while_loop.js
@@ -3,12 +3,11 @@ console.log('randomInteger=',randomInteger);
 
 let guessNumber=prompt('guess a number 0-50:');
 while(guessNumber!=randomInteger){
-    if(Math.abs(guessNumber-randomInteger)<=10){
+    if(guessNumber>randomInteger){
         console.log(' the guess was too high!');
     }else{
         console.log(' the guess was too low.');
     }
-
     guessNumber=prompt('guess a number 0-50:');
 }
 alert('Your guess was write!');
