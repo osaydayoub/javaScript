ad62fb09e3a37381b8b6e1c262099a1d048c41a8
osaydayoub <osaydayoub@gmail.com>
2023-09-21 10:57:08 +0300

    2.1 & 2.2

---
+++ functions/2.1_functions.js
@@ -0,0 +1,10 @@
+function countryInfo(country,population,capitalCity){
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+let spain=countryInfo('Spain',47,'Madrid');
+let canada=countryInfo('Canada',39,'Ottawa')
+let japan=countryInfo('Japan',125,'Tokyo')
+
+console.log(spain);
+console.log(canada);
+console.log(japan);
