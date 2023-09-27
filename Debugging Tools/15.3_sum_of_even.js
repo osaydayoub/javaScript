ba4fb895ed1301ce4e732f8ed1bd0a37f31f0256
osaydayoub <osaydayoub@gmail.com>
2023-09-27 23:02:48 +0300

    15.3

--- Debugging Tools/15.3_sum_of_even.js
+++ Debugging Tools/15.3_sum_of_even.js
@@ -4,7 +4,7 @@ function getSumOfEven(arr){
     }
    console.log(getSumOfEven([1,2,3,4,5,6,7,8,9,10]));
    // there is tow problems in line 2-3:
-   //in line 3 access to an array with an index out of range arr[10]
+   //in line 3 access to an array with an index out of range (arr[10])
    // so we get NaN, the range of index is 0-9,
    //so to get the value of the even numbers we have to go to arr[1]+arr[3]...
     
