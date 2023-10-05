5b91c911d907555fdc7f26e282d4d37c4751bbd0
osaydayoub <osaydayoub@gmail.com>
2023-10-05 09:57:32 +0300

    19.2

---
+++ Traversing and Creating Nodes/19.2/obj.js
@@ -0,0 +1,43 @@
+const users = [
+    {
+    id: 167464,
+    firstName: "Jimmy",
+    lastName: "Arnold",
+    email: "jimmya@gmail.com",
+    },
+    {
+    id: 578447,
+    firstName: "Martha",
+    lastName: "Goldman",
+    email: "gold@hotmail.com",
+    },
+    {
+    id: 864578,
+    firstName: "Tim",
+    lastName: "Burton",
+    email: "timmy.hotmail.com",
+    },
+    ];
+// 1. Loop over the array with the forEach method and
+// dynamically create an ordered list of the first and last
+// names of the objects.
+// 2. Remove the bullet points of the ordered list with
+// JavaScript.
+// 3. Create a custom data attribute called “data-id” and attach
+// the id value to each li.
+    //ordered list
+    const body=document.querySelector('body'); 
+    const myNewOl = document.createElement('ol');
+    //let liText = document.createTextNode("sub title 4");
+    //myNewLi.appendChild(liText)
+    body.appendChild(myNewOl);
+    let newLi;
+    let text='';
+    users.forEach((user)=>{
+        text=document.createTextNode(`${user.firstName} ${user.lastName}`)
+        newLi=document.createElement('li');
+        newLi.appendChild(text);
+        newLi.dataset.id=`${user.id}`
+        myNewOl.appendChild(newLi);
+    })
+    myNewOl.style.listStyleType="none";
